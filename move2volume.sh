4e7f7b1143c0f8f13eba9b559c96efe3320b3d5f
Pete Canfield <cpete@wustl.edu>
2024-05-16 12:22:20 -0500

    Clarified -v

--- move2volume.sh
+++ move2volume.sh
@@ -6,7 +6,7 @@ volume_name="DBN_DATA"
 print_usage() {
   printf "\n\nUsage:\n\n"
   printf "\tParameters:\n\n"
-  printf "\t %s\n\n" "[-d]: Specify the source data directory and move to volume." "[-y]: Copy the volume output data to host machine at this directory." "[-c]: Clean the volume directory." "[-p]: List the directory information of the volume." "[-v]: Specify the name of the volume you want to write to or read from (DBN_DATA is default)" "[-h]: Print this menu." 
+  printf "\t %s\n\n" "[-d]: Specify the source data directory and move to volume." "[-y]: Copy the volume output data to host machine at this directory." "[-c]: Clean the volume directory." "[-p]: List the directory information of the volume." "[-v]: Specify the name of the volume you want to write to or read from (DBN_DATA is default). THIS MUST PRECEED ANY OTHER OPTIONS." "[-h]: Print this menu." 
   exit 1
 }
 
@@ -16,29 +16,30 @@ clean_volume() {
 }
 
 print_volume() {
-    docker run --rm -i -v=${volume_name}:/volume busybox find /volume
+    echo ${volume_name}
+    docker run --rm -i -v ${volume_name}:/volume busybox find /volume
 }
 
 copy_to_volume () {
-    docker container create --name move -v ${volume_name}:/root hello-world ;
-    docker cp $host_dir move:/root ;
-    docker rm move ;
+    docker container create --name move -v ${volume_name}:/root hello-world 
+    docker cp $host_dir move:/root 
+    docker rm move 
 }
 
 
 copy_to_host () {
-    docker container create --name move -v ${volume_name}:/root hello-world ;
-    docker cp move:/root/ $host_dir ;
-    docker rm move ;
+    docker container create --name move -v ${volume_name}:/root hello-world 
+    docker cp move:/root/ $host_dir 
+    docker rm move 
 }
 
 
 while getopts 'puhcd:y:v:' flag; do
   case "${flag}" in
-    d) echo $OPTARG ;
-       host_dir=$OPTARG ; 
+    d) echo $OPTARG 
+       host_dir=$OPTARG 
        copy_to_volume ;;
-    y) host_dir=$OPTARG ;
+    y) host_dir=$OPTARG 
        copy_to_host ;;
     v) volume_name=$OPTARG ;;
     c) clean_volume ;;
